f43e01c222e624e23091bff2cac7be59e6cedc9f
Danilo  Prieto <danilo.s.prieto@gmail.com>
2021-03-30 09:38:10 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Data{
+  h:number,
+  w:number,
+  depth:number
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calcubyte';
+  public bits: number = 0;
+  public bytes: number = 0;
+  public kbytes: number = 0;
+  public mbytes: number = 0;
+
+  public data: Data ={
+    h:0,
+    w:0,
+    depth:0
+  };
+
+  constructor(){}
+
+  calculator(){
+      let res: number = ( this.data.h * this.data.w ) * this.data.depth;
+
+      this.bits = res;
+      this.bytes = this.bits / 8;
+      this.kbytes = this.bytes / 1024;
+      this.mbytes = this.kbytes / 1024;
+  }
 }
+
+
